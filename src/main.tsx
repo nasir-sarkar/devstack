298a94fe3e35f13bd1ae37bbc594433dc3976d57
nasir-sarkar <nasirsarkar537@gmail.com>
2026-09-13 01:04:42 +0600

    change notification position

--- src/main.tsx
+++ src/main.tsx
@@ -10,6 +10,6 @@ import "react-toastify/dist/ReactToastify.css";
 createRoot(document.getElementById("root")!).render(
   <StrictMode>
     <App />
-    <ToastContainer />
+    <ToastContainer position="top-center"/>
   </StrictMode>,
 );
